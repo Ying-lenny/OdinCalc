7f40be98efe1259bb2c0395c0227769c5656ba9a
Owen Lenihan <owenlenihan101@gmail.com>
2022-05-26 00:14:33 +0100

    Added unused Evaluate feature
    Added a new button event listener and evaluation function which will be expanded later

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const numberButtons = document.querySelectorAll('[data-number]')
 const operatorButtons = document.querySelectorAll('[operator-button]')
 const clearButton = document.getElementById('clear-button')
 const deleteButton = document.getElementById('delete-button')
+const equalButton = document.getElementById('equal-button')
 const currentOpScreen = document.getElementById('current-op-screen')
 const previousOpScreen = document.getElementById('previous-op-screen')
 
@@ -15,6 +16,8 @@ const previousOpScreen = document.getElementById('previous-op-screen')
 clearButton.addEventListener('click', resetScreen)
 //Triggers Delete function on button click
 deleteButton.addEventListener('click', deleteNumber)
+//Triggers equal function on button
+equalButton.addEventListener('click', evaluate)
 
 //Triggers function to add operator to calculation
 operatorButtons.forEach((button) =>
@@ -48,6 +51,11 @@ function resetScreen() {
     currentOpScreen.textContent = ""
     previousOpScreen.textContent = ""
     currentOp = null
+}
+
+//Starts Evaluation to see if a valid calculation was submitted
+function evaluate() {
+    console.log("Evaluated")
 }
 
 //Removes most recent number from current calculation
